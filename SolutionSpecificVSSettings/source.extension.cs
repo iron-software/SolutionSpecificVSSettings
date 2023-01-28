97397f893ce3f6cdd96cc43ef7a6838f339d5217
Artem Koloskov <artem@ironsoftware.com>
2023-01-28 13:15:58 +0700

    Fix Id

--- SolutionSpecificVSSettings/source.extension.cs
+++ SolutionSpecificVSSettings/source.extension.cs
@@ -7,7 +7,7 @@ namespace SolutionSpecificVSSettings
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "df2bbc04-f83d-4b8a-95f9-2612666a2757";
+        public const string Id = "7A7A54CF-455F-447C-9CF6-6E75075BCBD9";
         public const string Name = "Solution Specific VSSettings";
         public const string Description = @"Imports a custom .vssettings on solution load.";
         public const string Language = "en-US";
